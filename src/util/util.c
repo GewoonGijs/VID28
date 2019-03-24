206ee4402ca7437fff4fd9031f027d1edeb3905e
Gijs Withagen <gijsw@Gijss-Air.fritz.box>
2019-03-24 23:20:19 +0100

    Cleaned it up and restructured it as a library.

---
+++ src/util/util.c
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <util/atomic.h>
+#include "util.h"
+
+void setMillis(unsigned long ms)
+{
+    extern unsigned long timer0_overflow_count;
+    ATOMIC_BLOCK (ATOMIC_RESTORESTATE) {
+        timer0_overflow_count = ms;
+    }
+}
+
+void setPrescaler(int timer, int divisor) {
+  byte mode;
+  if(timer == 0 || timer == 1) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(timer == 0) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(timer == 2) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x07; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
+}
---
+++ src/util/util.h
@@ -0,0 +1,10 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setPrescaler(int timer, int divisor);
+void setMillis(unsigned long ms);
+
+#ifdef __cplusplus
+}
+#endif
